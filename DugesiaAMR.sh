188209cdb4960a2d2c1901956cb4ee7de10b2e71
zblara <72982331+zblara@users.noreply.github.com>
2021-04-27 14:03:21 +0800

    Add files via upload

---
+++ DugesiaAMR.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+#insert SLURM/sbatch variables here if any
+ 
+echo "working directory = "$SLURM_SUBMIT_DIR 
+
+ulimit -s unlimited 
+
+NAME=G1
+
+cd /path/to/raw/data &&\
+
+fastp -i "$SAMPLE"_1.fq.gz -I "$SAMPLE"_2.fq.gz -o "$SAMPLE"_1_fastp.fq.gz -O "$SAMPLE"_2_fastp.fq.gz -q 33 -l 30 -w 88 -h "$SAMPLE"_fastp.html &&\
+
+bowtie2 --end-to-end --sensitive --no-discordant --no-mixed -t -p 88 -x megares -1 "$SAMPLE"_1.fq.gz -2 "$SAMPLE"_2.fq.gz -S "$SAMPLE"_bt2.sam &&\
+#make sure you have the megares bt2 index files in the working directory
+
+sed '1~2d' "$SAMPLE"_bt2.sam | cut -f1,3,4,5,8 | grep -v '*' > /$SAMPLE.meg.drs && \
+perl -w procSam.pl $SAMPLE.meg.drs megares.lengths > $SAMPLE.meg.csv
